a78915eedc2353695aa14d9c7620e023ad3c22ab
paul <paulosullivan22@outlook.com>
2019-05-23 18:34:45 +0100

    restructured file system

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Card from './Card';
+import { quotes } from './Quotes';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        id: 1
+    }
+  }
+  
+
+
+
+    render() {
+      return (
+        <div className="tc">
+          <h1>Cheesy inspirational quotes</h1>
+          <button
+            className="button"
+            onClick={changeQuote}>
+            Next quote
+            </button>
+          <Card quotes={quotes}/>
+        </div>
+      )
+    }
+    changeQuote() {
+      this.setState(function() {
+        return {
+          id: 2
+          }
+        })
+      }
+  }
+
+// const App = () => {
+//   return (
+//     <div className='tc'>
+//       <h1>Cheesy inspirational quotes</h1>
+//       <CardList quotes={quotes}/>
+//     </div>
+//   )
+// }
+
+
+export default App;
